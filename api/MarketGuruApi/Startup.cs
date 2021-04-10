6d2c51d4062433cdfb2c386ac59dbf9d65e25ab1
AP <Flank2k2@users.noreply.github.com>
2021-04-09 20:59:00 -0700

    Hotfix/cors (#3)
    * Create branch to figure out CORS
    
    * Note to self: remove trailling / on cors config
    
    * Remove other changes
    
    * Remove more debuging code
    
    * More tests
    
    * Update deploy-cloud-run.yml
    
    * Update deploy-gh-page.yml
    
    Co-authored-by: Antoine Pouelt <antoine.poulet@rouseservices.com>

--- api/MarketGuruApi/Startup.cs
+++ api/MarketGuruApi/Startup.cs
@@ -1,18 +1,11 @@
+using MarketGuru.Core.Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using MarketGuru.Core.Microsoft.Extensions.DependencyInjection;
 
 namespace MarketGuruApi
 {
